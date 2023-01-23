c0adb54ac84e8d6bdc99b1f4fa74a10c6d720ff3
Ehedei Hernandez <ehernandez@eturia.es>
2023-01-23 10:32:52 +0000

    Topic 7

---
+++ main.py
@@ -0,0 +1,9 @@
+from calculadora import sumar, multiplicar, restar, dividir
+
+a = 9
+b = 4
+
+print(f'Resultado de sumar {a} y {b}: {sumar(a, b)}')
+print(f'Resultado de restar {a} y {b}: {restar(a, b)}')
+print(f'Resultado de multiplicar {a} y {b}: {multiplicar(a, b)}')
+print(f'Resultado de dividir {a} por {b}: {dividir(a, b)}')
